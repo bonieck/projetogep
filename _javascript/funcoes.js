de653589cc986ce5653df2c44efdf7f92bbd16a2
Bonieck Douglas <bonieck.douglas@hotmail.com>
2022-09-22 22:20:22 -0300

    primeiro commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,26 @@
+	function mudaFoto (foto) {
+		document.getElementById('icone').src = foto;
+	}
+/*
+	var tinta = false;
+     function mudaLampada(tipo) {
+	     	if (tipo == 1) {
+	     		arquivo = "_imagens/caveira.png";
+	     	}
+	     	if (tipo == 2) {
+	     		arquivo ="_imagens/caveira1.png";
+	     	}
+	     	if (tipo == 3) {
+	     		arquivo ="_imagens/caveira2.png";
+	     		tinta = true;
+	     	}
+	     	if (!tinta) { 
+	     		
+	     	document.getElementById("img").src= arquivo;
+	     	if (tipo == 3) {
+	     		tinta = true;
+	     	}
+
+	     }
+
+*/
